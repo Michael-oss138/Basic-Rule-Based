731f0eec8ca4df70febb43271cbfa41c0ffca217
Michael-oss138 <nwaizumichael0@gmail.com>
2026-05-11 23:54:09 +0100

    done

--- app.py
+++ app.py
@@ -1,16 +1,61 @@
 from flask import Flask, request, jsonify
 from classifier import classify_job
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/classify", methods=["POST"])
 def classify():
     data = request.get_json()
-    title = data.get("title", "")
 
-    result = classify_job(title)
+    jobs = data.get("jobs", "")
 
-    return jsonify(result)
+    results = []
+    for job in jobs:
+        title = job.get("title", "")
+
+        result = classify_job(title)
+
+        results.append(result)
+
+    return jsonify({
+        "results": results
+    })
+
+
+@app.route("/upload-csv", methods=["POST"])
+def upload_csv():
+
+    if "file" not in request.files:
+        return jsonify({
+            "error": "No file uploaded"
+        }), 400
+
+    file = request.files["file"]
+
+    try:
+        df = pd.read_csv(file)
+
+        results = []
+
+        for _, row in df.iterrows():
+
+            title = str(row["title"])
+
+            result = classify_job(title)
+
+            results.append(result)
+
+        return jsonify({
+            "total_jobs": len(results),
+            "results": results
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        }), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
